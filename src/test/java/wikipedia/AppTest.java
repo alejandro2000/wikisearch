6997f425efcbda88de9abc00075f3491750f9c6f
Alejandro Taborda Cadavid <gammax136@gmail.com>
2020-09-15 12:05:29 -0500

    setting up the chrome driver

--- src/test/java/wikipedia/AppTest.java
+++ src/test/java/wikipedia/AppTest.java
@@ -1,20 +1,26 @@
 package wikipedia;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws InterruptedException {
+        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
     }
 }
